4a78ae4331741006f12e2652cdee95471bd39601
NemethAngela <nemeth@gmail.com>
2023-05-03 15:37:53 +0200

    Kezdes

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        new MainFrame();    //csak le kell futnia a konstruktornak, ez a névtelen példány
+    }
+}
+
+//System.out.println("Hello, World!");
---
+++ src/MainFrame.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame {
+    InputPanel tomegPanel;
+    InputPanel magassagPanel;
+    InputPanel indexPanel;
+    JPanel buttonPanel;
+        
+    public MainFrame() {
+        this.setFrame();    //nem itt állítjuk  be az ablakot, a konstruktor nem erre való, itt csak meghívjuk
+        this.initComponent();
+        this.addComponent();
+        
+    }
+
+    private void initComponent() {
+        this.tomegPanel = new InputPanel("Tömeg");
+        this.magassagPanel = new InputPanel("Magassag");
+        this.buttonPanel = new JPanel();
+        this.indexPanel = new InputPanel("Testtömeg index");
+    }
+
+    private void addComponent() {
+        this.add(this.tomegPanel);
+        this.add(this.magassagPanel);
+        this.add(this.buttonPanel);     //nem mindegy a sorrend
+        this.add(this.indexPanel);
+    }
+
+    private void setFrame() {
+        this.setLayout (new BoxLayout(this.getContentPanel(), BoxLayout.PAGE_AXIS));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //enélkül nem lép ki 
+        //this.setSize(300, 200);
+        this.pack();    // így felveszi a komponens nevét, nem kell méretezgetni
+        this.setVisible(true);
+    }
+}
